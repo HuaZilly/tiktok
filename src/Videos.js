590a01addc880f6dfefefe4efdbc71c243303c93
Thanh Zill Pentakill <thanh.hua@balanceinterent.com.au>
2024-10-26 19:46:13 +0700

    useImperativeHandle

--- src/Videos.js
+++ src/Videos.js
@@ -1,6 +1,7 @@
-import video1 from './video/video-1.mp4';
+import video1 from './Video/video-1.mp4';
 import {forwardRef, useImperativeHandle, useRef} from 'react';
-
+import styles from './Video/Video.module.scss'
+import {clsx} from "clsx";
 const Videos = (props, currentRef) => {
     const videoRef = useRef();
     useImperativeHandle(currentRef, () => ({
@@ -10,9 +11,17 @@ const Videos = (props, currentRef) => {
         pause() {
             videoRef.current.pause()
         }
-    }))
+    }));
+    const value = {
+        isValue: 1
+    };
+    const test = {
+        [value.isValue]: 213
+    }
+    console.log(test)
     return (
         <>
+            <h2 className={clsx(styles.heading, styles.cRed)}> Heading 2 <span>test 123</span></h2>
             <video
                 src={video1}
                 width={280}
