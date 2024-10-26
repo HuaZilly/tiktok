05db4fa2b8218d7b78a67db43437accb2af2506f
Thanh Zill Pentakill <thanh.hua@balanceinterent.com.au>
2024-10-26 20:56:06 +0700

    Update dependency

--- src/App.js
+++ src/App.js
@@ -1,61 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import {useStore, StoreContext, StoreProvider, actions} from "./store";
-import {useRef} from "react";
-import Videos from  './Videos';
 
 function App() {
-    const [state, dispatch] = useStore();
-    const {todos, todoInput} = state;
-
-    const handleAdd = () => {
-        dispatch(actions.addTodo(todoInput));
-        inputRef.current.focus();
-    }
-
-    const inputRef = useRef()
-
-    console.log(todos)
-
-
-    const videoRef = useRef();
-
-    const handlePause = () => {
-        videoRef.current.pause();
-    }
-    const handlePlay = () => {
-        videoRef.current.play();
-    }
     return (
         <div className="App">
-            <Videos ref={videoRef}/>
-            <button onClick={handlePlay}> Play</button>
-            <button onClick={handlePause}> Pause</button>
-            <div>
-                <input
-                    ref={inputRef}
-                    value={todoInput}
-                    placeholder={'Enter Todo'}
-                    onChange={e => {
-                        dispatch(actions.setTodoInput(e.target.value))
-                    }}
-                />
-                <button onClick={handleAdd}>
-                    Add
-                </button>
-                {
-                    todos?.length > 0 && (
-                        <ul>
-                            {
-                                todos?.map((todo, index) => (
-                                    <li key={index}> {todo} </li>
-                                ))
-                            }
-                        </ul>
-                    )
-                }
-
-            </div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
         </div>
     );
 }
